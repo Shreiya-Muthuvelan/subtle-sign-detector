82baa77dc95c20ebcc277a579a204de63ce72dca
Shreiya-Muthuvelan <shreiyavel@gmail.com>
2025-04-18 19:55:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import joblib
 import numpy as np
 st.set_page_config(page_title="Subtle Sign Detector", page_icon="🧠")
-
-# Load models
 regressor = joblib.load('burnout_regressor.pkl')
 def categorize_risk(burn_rate):
     if burn_rate < 0.3:
@@ -13,14 +11,9 @@ def categorize_risk(burn_rate):
     else:
         return "High"
 
-# App Title
-#st.set_page_config(page_title="Subtle Sign Detector", page_icon="🧠")
 st.title("🧠 Subtle Sign Detector")
 st.subheader("Detect burnout levels and receive mental wellness suggestions")
-
 st.markdown("---")
-
-# Input Fields
 st.markdown("#### 📥 Input Details")
 mental_fatigue = st.slider("🧠 Mental Fatigue Score", 0.0, 10.0, 5.0)
 resource_alloc = st.slider("⚙️ Resource Allocation (1-10)", 1, 10, 5)
@@ -28,26 +21,14 @@ days_since_joining = st.number_input("📅 Days Since Joining", min_value=0, val
 
 st.markdown("---")
 
-# Predict Button
 if st.button("🔍 Predict Burnout"):
     input_data = np.array([[resource_alloc, mental_fatigue, days_since_joining]])
-
-    # Model Predictions
     burn_rate = regressor.predict(input_data)[0]
-    # Map numeric label to string
     risk_label_numeric = categorize_risk(burn_rate)
-    
-
-
-
-
     st.markdown(f"### 🔥 Predicted Burn Rate: `{burn_rate:.2f}`")
     st.markdown(f"### 😫 Burnout Risk Level: `{risk_label_numeric}`")
     st.markdown("---")
-
-    # 🧘 Suggestions Based on Risk Level
     st.markdown("### 💡 Mental Wellness Suggestions:")
-
     if risk_label_numeric == "Low":
         st.success("""
         - ✅ Keep up your healthy routine  
